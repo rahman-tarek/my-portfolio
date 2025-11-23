555103bff8c83a039ca33d87eeb56e848c291407
MD Tarek Rahman <mdtarekrahman209@gmail.com>
2025-11-23 10:00:37 +0600

    feat: add resume button and drive link

--- src/components/ResumeDownloader.tsx
+++ src/components/ResumeDownloader.tsx
@@ -1,11 +1,15 @@
 import { FaFileAlt } from "react-icons/fa";
+import { Link } from "react-router";
 
 const ResumeDownloader = () => {
     return (
-        <div
-            className="text-xs md:text-sm flex justify-center items-center gap-2 bg-gray-200 px-3 mt-3 py-1 rounded-full cursor-pointer"
-        >
-            <FaFileAlt /> Download Resume
+        <div>
+            <Link
+                to={`https://drive.google.com/file/d/1lfNHSjB8PyWjGfdO3dzj31HNDwCN6MDr/view?usp=sharing`}
+                className="text-xs md:text-sm flex justify-center items-center gap-2 bg-gray-200 px-3 mt-3 py-1 rounded-full cursor-pointer"
+            >
+                <FaFileAlt /> Download Resume
+            </Link>
         </div>
     )
 }
